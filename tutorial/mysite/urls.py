ac4a17ef38dc424cd7266dcc0ef8f5d678e5a2c0
Lucky Boy <127705766+LcckyBoyy@users.noreply.github.com>
2025-05-17 14:54:25 +0700

    pre-deploy

--- tutorial/mysite/urls.py
+++ tutorial/mysite/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url='/polls/', permanent=True)),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ] + debug_toolbar_urls()
